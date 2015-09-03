826d61c86329576c5b85862e3f1cb9e75c77491e
Eric Gjerde <eric.gjerde@mymail.nwtc.edu>
2015-09-03 16:42:22 -0500

    Shapes project, initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Shapes
 Shapes Assignment for Programming 3 (Java)
+
+Draws a box, an oval, an arrow and a diamond using asterisks to the standard console
---
+++ src/com/liquidforte/java/Shapes.java
@@ -0,0 +1,36 @@
+package com.liquidforte.java;
+
+/**
+ * Shapes.java - Shapes class<br />
+ * Prints out a box, oval, arrow and diamond using asterisks to the standard
+ * console.
+ * 
+ * @author Eric Gjerde
+ * @version 1.0.0 September 3, 2015
+ */
+public class Shapes {
+    // Hide constructor from javadoc
+    private Shapes() {
+    }
+
+    /**
+     * Main method - do the heavy lifting
+     * 
+     * @param args
+     *            The command line arguments/parameters
+     */
+    public static void main(String[] args) {
+        System.out.println("*********                ***                *                  *                ");
+        System.out.println("*       *              *     *             ***               *   *                ");
+        System.out.println("*       *            *         *          *****            *       *            ");
+        System.out.println("*       *            *         *            *            *           *            ");
+        System.out.println("*       *            *         *            *          *               *        ");
+        System.out.println("*       *            *         *            *        *                   *        ");
+        System.out.println("*       *            *         *            *          *               *        ");
+        System.out.println("*       *            *         *            *            *           *            ");
+        System.out.println("*       *            *         *            *              *       *            ");
+        System.out.println("*       *              *     *              *                *   *                ");
+        System.out.println("*********                ***                *                  *                ");
+    }
+
+}
